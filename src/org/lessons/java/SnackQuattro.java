dabbc8da96fab106b9a4d6882c5801b9d04e102f
mirkettinho <mirkodifranco30@gmail.com>
2023-09-14 14:35:06 +0200

    snack 4 completed

--- src/org/lessons/java/SnackQuattro.java
+++ src/org/lessons/java/SnackQuattro.java
@@ -7,11 +7,29 @@ public class SnackQuattro {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Inserisci una parola: ");
+		///I ask the user to enter a word
+		System.out.print("Inserisci una parola: ");
 		String word = sc.nextLine().toLowerCase().trim();
 		
 		sc.close();
 		
-		///DA COMPLETARE
+		///I create an array of characters of the same length as the word entered by the user
+		char[] wordArray = new char[word.length()];
+		///I take the single character
+		for (int i = 0; i < word.length(); i++ ) {
+			wordArray[i] = word.charAt(i);
+		}
+		
+		///reverse the word
+		String wordRevers = "";
+		for (int i = wordArray.length -1; i > -1; i--) {
+			wordRevers += wordArray[i];
+		}
+		
+		if(word.equals(wordRevers)) {
+			System.out.println("é palindroma");
+		}else {
+			System.out.println("Non é palindroma");
+		}
 	}
 }
