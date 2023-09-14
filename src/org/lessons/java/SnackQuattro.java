57eb2cd79cec82bcc3b5f270a3bac8566e3e3255
mirkettinho <mirkodifranco30@gmail.com>
2023-09-14 12:43:46 +0200

    snack 3 completed and snack 4 in progress

---
+++ src/org/lessons/java/SnackQuattro.java
@@ -0,0 +1,17 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class SnackQuattro {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Inserisci una parola: ");
+		String word = sc.nextLine().toLowerCase().trim();
+		
+		sc.close();
+		
+		///DA COMPLETARE
+	}
+}
--- src/org/lessons/java/SnackTre.java
+++ src/org/lessons/java/SnackTre.java
@@ -1,5 +1,20 @@
 package org.lessons.java;
 
+
 public class SnackTre {
-	
+	public static void main(String[] args) {
+		
+		int[] numbers = {1,2,3,4,5,6,7,8,9,11,12,13,14,15};
+		
+		int sum = 0;
+		
+		for (int i = 0; i < numbers.length; i ++) {
+			
+			if (i % 2 != 0 ) {
+				sum += numbers[i];
+			}
+		}
+		
+		System.out.println("La somma dei numeri con posizione dispari é: " + sum);
+	}
 }
