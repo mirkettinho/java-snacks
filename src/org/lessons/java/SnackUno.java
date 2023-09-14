203646a8e9fc0d1da629330d3ee523ec5324de4d
mirkettinho <mirkodifranco30@gmail.com>
2023-09-14 11:47:47 +0200

    snack 1 completed

--- src/org/lessons/java/SnackUno.java
+++ src/org/lessons/java/SnackUno.java
@@ -1,5 +1,21 @@
 package org.lessons.java;
 
-public class SnackUno {
+import java.util.Scanner;
 
+public class SnackUno {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero: ");
+		int num = sc.nextInt();
+		
+		sc.close();
+		
+		if(num % 2 == 0) {
+			System.out.println(num);
+		}else {
+			System.out.println(num +1);
+		}
+	}
 }
